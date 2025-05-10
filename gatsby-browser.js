8cfe0583d8913d848bacaf9b65afaa064da50a44
muhibarshad <muhibarshad123@gmail.com>
2025-05-11 01:27:10 +0500

    logo

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,7 +4,14 @@
 //   }
 // }
 
+// gatsby-browser.js
+
 import './src/styles/common/bulma.css'
 import './src/styles/common/style.css'
 import './src/styles/common/custom.css'
 import './src/styles/common/util.css'
+
+if (process.env.NODE_ENV === 'production') {
+  const { inject } = require('@vercel/analytics')
+  inject()
+}
