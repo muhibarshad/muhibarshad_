0ef5f95687dad284f570a0cd4115379947828c09
muhibarshad <muhibarshad123@gmail.com>
2025-05-11 01:38:13 +0500

    vercel

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -10,8 +10,3 @@ import './src/styles/common/bulma.css'
 import './src/styles/common/style.css'
 import './src/styles/common/custom.css'
 import './src/styles/common/util.css'
-
-if (process.env.NODE_ENV === 'production') {
-  const { inject } = require('@vercel/analytics')
-  inject()
-}
